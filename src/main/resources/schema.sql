9a89c744c5c982a756442a7f4b34666634fdb285
ALLIANCE-dieudonne <filalliance769@gmail.com>
2023-08-17 23:01:51 +0200

    adding database

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS Content(
+        id INTEGER AUTO_INCREMENT,
+        title varchar(300) NOT NULL,
+        desc text,
+        status VARCHAR(28) NOT NULL,
+        content_type VARCHAR(58) NOT NULL,
+        date_created TIMESTAMP NOT NULL,
+        date_updated TIMESTAMP,
+        url VARCHAR(25),
+        primary key (id)
+ );
+
+ INSERT INTO Content(title,desc,status,content_type,date_created)
+ VALUES('this is the title','this is funny','IDEA','ARTICLE',CURRENT_TIMESTAMP);
